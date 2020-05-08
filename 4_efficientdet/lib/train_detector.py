811bbf2e76a28c13eaa361cee0b8e3d0ad354b1e
hui qin <qinhui99@hotmail.com>
2020-05-08 13:50:55 +0800

    add the parameter: compound_coef
    so that we can set  my own the compound_coef parameter.  e.g.  compound_coef=0 ,we use the efficentdet0 network; compound_coef=1 ,we use the efficentdet1 network.

--- 4_efficientdet/lib/train_detector.py
+++ 4_efficientdet/lib/train_detector.py
@@ -99,9 +99,9 @@ class Detector():
                                                                 **self.system_dict["local"]["val_params"])
 
 
-    def Model(self,gpu_devices=[0]):
+    def Model(self,gpu_devices=[0],compound_coef=0):
         num_classes = self.system_dict["local"]["training_set"].num_classes();
-        efficientdet = EfficientDet(num_classes=num_classes)
+        efficientdet = EfficientDet(num_classes=num_classes,compound_coef=compound_coef)
 
         if self.system_dict["params"]["use_gpu"]:
             self.system_dict["params"]["gpu_devices"] = gpu_devices
